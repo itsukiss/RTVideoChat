013a293a92a07e6da92d9a751cd4409b5b09b1ab
Itsuki Tanaka <itsuki@Itsuki-no-MacBook-Pro.local>
2015-11-16 13:13:21 +0900

    first commit

---
+++ Search.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title>Bing</title>
+</head>
+<body>
+<?php 
+// $query = $_POST['word'];
+$query = 'ご飯';
+
+
+$accountKey = '5hj8WLaVWT33WCk04XwcZ0UFI8GLZLGuFXntSZUWwuA';
+$ServiceRootURL = 'https://api.datamarket.azure.com/Bing/Search/v1/';
+
+$context = stream_context_create( array(
+	'http'=>array(
+		'request_fulluri'=>TRUE,
+		'header'=>'Authorization: Basic '.base64_encode( $accountKey.':'.$accountKey)
+	)));
+
+$request = $ServiceRootURL.'Web?$format=json&Query='.urlencode( '\''.$query.'\'' );
+$response = file_get_contents( $request, FALSE, $context);
+
+//header( 'Content-Type: application/json' );
+echo $response;
+
+?>
+</body>
+</html>
+
+
